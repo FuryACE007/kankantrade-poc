2e52473127c2ffc951395b08f6a889f4f3e78cbb
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-10-09 00:15:20 +0530

    init commit

---
+++ src/main/java/com/skaledevs/kankantrade/KankantradeApplication.java
@@ -0,0 +1,13 @@
+package com.skaledevs.kankantrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KankantradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KankantradeApplication.class, args);
+	}
+
+}
